2f8ea42551c116a968ac7da2b0e49679a268093f
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-06-17 02:09:55 +0900

    fix Renamed main.swift to Wateco.swift. add Wateco's configuration, wave-to subcommand and text-to subcommand.

---
+++ WaveTextConverter/Wateco.swift
@@ -0,0 +1,47 @@
+//
+//  Wateco.swift
+//  WaveTextConverter
+//
+//  Created by Eisuke KASAHARA on 2023/06/12.
+//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct Wateco: ParsableCommand {
+    static let configuration = CommandConfiguration(
+            commandName: "wateco",
+            abstract: "Conversion tool for audio and text data",
+            discussion: """
+            This application can convert from audio file to text format or from text file to audio format.
+            """,
+            version: "1.0.0",
+            shouldDisplay: true,
+            subcommands: [WaveTo.self, TextTo.self],
+            helpNames: [.long, .short])
+    
+}
+
+extension Wateco {
+    struct WaveTo: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Convert audio file to text format")
+
+        @OptionGroup var outputFile: OutputFile
+        @OptionGroup var inputFile: InputFile
+        
+        mutating func run() {
+            print(inputFile.url)
+        }
+    }
+}
+
+extension Wateco {
+    struct TextTo: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Convert text file to audio format")
+        
+        @OptionGroup var outputFile: OutputFile
+        @OptionGroup var inputFile: InputFile
+    }
+}
+
