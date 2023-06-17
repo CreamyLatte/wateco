c7de99bd1db4bb897cad547710e7d1436c4fa39e
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-06-17 09:29:09 +0900

    add AVFormat.swift.

---
+++ WaveTextConverter/AVFormat.swift
@@ -0,0 +1,16 @@
+//
+//  AVFormat.swift
+//  wateco
+//
+//  Created by Eisuke KASAHARA on 2023/06/17.
+//
+
+import Foundation
+
+enum AVFormat: String {
+    case wav
+    case aiff
+    case appleLossless = "alac"
+    case aac
+    case mp3
+}
