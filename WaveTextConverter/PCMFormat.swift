81a9b925af27fd72607ac0a9d01eccf78c5750b2
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-06-19 00:07:50 +0900

    add PCMFormat.swift.

---
+++ WaveTextConverter/PCMFormat.swift
@@ -0,0 +1,33 @@
+//
+//  PCMFormat.swift
+//  wateco
+//
+//  Created by Eisuke KASAHARA on 2023/06/19.
+//
+
+import Foundation
+import AVFoundation
+import ArgumentParser
+
+enum PCMFormat: String {
+    case float32
+    case float64
+    case int16
+    case int32
+    
+    var audioCommonFormat: AVAudioCommonFormat {
+        switch self {
+        case .float32:
+            return .pcmFormatFloat32
+        case .float64:
+            return .pcmFormatFloat64
+        case .int16:
+            return .pcmFormatInt16
+        case .int32:
+            return .pcmFormatInt32
+        }
+    }
+}
+
+extension PCMFormat: CaseIterable, ExpressibleByArgument {
+}
