9cb9784d21b14fae2b73c98ddf9a979892db51b1
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-10-19 20:49:55 +0900

    Package 内容の修正

--- Package.swift
+++ Package.swift
@@ -4,11 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "WaveTextConverter",
+    name: "wateco",
+    platforms:[.macOS(.v11)],
+    dependencies: [
+        // other dependencies
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.2.3")),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "WaveTextConverter"),
+        .executableTarget(name: "wateco", dependencies: [
+            // other dependencies
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        ]),
+        // other targets
     ]
 )
